e4632b670b1fe99da34b59e01ede3658e1e865b3
chenrunhu <604594903@qq.com>
2018-07-07 22:23:14 +0800

    tj

--- scrapy_wssc/spiders/ls_wb_spider.py
+++ scrapy_wssc/spiders/ls_wb_spider.py
@@ -19,7 +19,7 @@ class ls_wb_spider(scrapy.Spider):
         self.allowed_domain = 'www.qu.la'
         self.driver = webdriver.Chrome(
             executable_path="C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe")
-        self.driver.set_page_load_timeout(10)  # throw a TimeoutException when thepage load time is more than 5 seconds.
+        self.driver.set_page_load_timeout(5)  # throw a TimeoutException when thepage load time is more than 5 seconds.
 
     def parse(self, response):
         """模拟浏览器实现翻页，并解析每一个话题列表页的url_list
@@ -30,16 +30,19 @@ class ls_wb_spider(scrapy.Spider):
             wait = WebDriverWait(self.driver, 5)
             wait.until(
                 #  等价于 def getA(x) {  return  x.find_element_by_xpath('//ul[@class="post-list"]/li[@class]/a')) }
-                lambda x: x.find_element_by_xpath('//ul[@class="post-list"]/li[@class]/a'))  # VIP，内容加载完成后爬取
-            sel_list = self.driver.find_elements_by_xpath('//ul[@class="post-list"]/li[@class]/a')
-            url_list = [sel.get_attribute("href") for sel in sel_list]
-            url_set |= set(url_list)
-            try:
-                wait = WebDriverWait(self.driver, 10)
-                wait.until(lambda driver: driver.find_element_by_xpath(
-                    '//ul[@class="pg1"]/li[@class="pg_next"]'))  # VIP，内容加载完成后爬取
-                next_page = self.driver.find_element_by_xpath('//ul[@class="pg1"]/li[@class="pg_next"]')
-                next_page.click()  # 模拟点击下一页
-            except:
-                print "#####Arrive thelast page.#####"
-                break
+                lambda x: x.find_element_by_xpath('//div[@id="newscontent"]/div[@class="1"]/ul'))  # VIP，内容加载完成后爬取
+            sel_list = self.driver.find_elements_by_xpath('//div[@id="newscontent"]/div[@class="1"]/ul/li/span[@class="s2"]/a')
+            for sel in sel_list:
+                print sel.get_attribute("href")
+            # url_list = [sel.get_attribute("href") for sel in sel_list]
+            # print(url_list)
+            # url_set |= set(url_list)
+            # try:
+            #     wait = WebDriverWait(self.driver, 10)
+            #     wait.until(lambda driver: driver.find_element_by_xpath(
+            #         '//ul[@class="pg1"]/li[@class="pg_next"]'))  # VIP，内容加载完成后爬取
+            #     next_page = self.driver.find_element_by_xpath('//ul[@class="pg1"]/li[@class="pg_next"]')
+            #     next_page.click()  # 模拟点击下一页
+            # except:
+            #     print "#####Arrive thelast page.#####"
+            #     break
